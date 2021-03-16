8c74d754a652b2317e222b936a88f1108e0f4d45
Ilya <ilyabikmeev@gmail.com>
2021-03-16 12:28:07 +0500

    Commit

---
+++ bot.py
@@ -0,0 +1,46 @@
+import telebot
+import config
+import random
+import parse
+
+from telebot import types
+
+bot = telebot.TeleBot(config.TOKEN)
+
+
+
+f = open('woman.txt','r')
+answers = list()
+for p in f:
+    answers.append(p)
+
+
+@bot.message_handler(commands = ['start','help'])
+def welcome(message):
+    sti = open('stickers/3.tgs','rb')
+    bot.send_sticker(message.chat.id, sti)
+
+    # keyboard
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("Статистика распространения коронавируса")
+
+    markup.add(item1)
+
+    bot.send_message(message.chat.id, "Добро пожаловать, {0.first_name}!\nЯ - <b>{1.first_name}</b>, создана, чтобы служить тебе, мой господин :3.".format(message.from_user, bot.get_me()),
+        parse_mode='html',reply_markup=markup)
+
+
+@bot.message_handler(content_types = ['text'])
+def lalala(message):
+    if message.chat.type == 'private':
+        if message.text == 'Статистика распространения коронавируса':
+            sti = open("stickers/5.tgs",'rb')
+            bot.send_sticker(message.chat.id, sti)
+            bot.send_message(message.chat.id,parse.message,parse_mode = 'html')
+        else:
+            sti = open("stickers/10.tgs",'rb')
+            bot.send_sticker(message.chat.id, sti)
+            bot.send_message(message.chat.id,answers[random.randint(0,len(answers)-1)])
+#RUN
+
+bot.polling(none_stop=True)
---
+++ parse.py
@@ -0,0 +1,55 @@
+import requests
+from bs4 import BeautifulSoup
+
+#Прогноз погоды
+link = 'https://xn--80aesfpebagmfblc0a.xn--p1ai/information/'
+r = requests.get(link).text
+
+soup = BeautifulSoup(r,'lxml')
+block = soup.find('small')
+
+#По состоянию на date
+date = block.get_text()
+
+def removeSymbols(stats):
+    
+    stats = stats.replace('"','')
+    stats = stats.replace('{','')
+    stats = stats.replace(' ','')
+    stats = stats.replace(':',' ')
+    stats = stats.replace('}',' ')
+    return stats
+
+cv_stats_virus = soup.find('cv-stats-virus')
+#print(cv_stats_virus[':stats-data'])
+stats = cv_stats_virus[':stats-data']
+stats = removeSymbols(stats)
+
+array = stats.split(',')
+
+def getData(array,number):
+    return array[number][array[number].find(' ') + 1 : len(array[number])]
+
+def getWholeData():
+    result = dict()
+    result['sick'] = getData(array,0)
+    result['sickChange'] = getData(array,1)
+    result['healed'] = getData(array,2)
+    result['healedChange'] = getData(array,3)
+    result['died'] = getData(array,4)
+    result['diedChange'] = getData(array,5)
+    return result
+
+data = getWholeData()
+message = '<b>' + date + '</b>' 
+message += ':\n<i>Выявлено случаев всего:</i> ' 
+message += "<b>" + data['sick'] + "</b>" 
+message += '\n<i>Выявлено случаев за сутки: </i><b>' 
+message += data['sickChange'] + '</b>\n<i>Человек выздоровело всего: </i><b>' 
+message += data['healed'] + "</b>\n"
+message += "<i>Человек выздоровело за сутки: </i>"
+message += "<b>" + data['healedChange'] + "</b>\n"
+message += "<i>Умерло всего:</i> "
+message += "<b>" + data['died']  + "</b>\n"
+message += "<i>Умерло за сутки:</i> "
+message += "<b>" + data['diedChange']  + "</b>"
